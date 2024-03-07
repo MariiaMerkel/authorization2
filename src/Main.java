bb4f4906081b955c240cc89f7670929b06619e89
Mariia <mariia_merkel@mail.ru>
2024-03-07 11:28:47 +0300

    убрала вложенности и добавила проверку на цифры

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
-import excaptions.WrongLoginException;
-import excaptions.WrongPasswordException;
+import excaptions.services.Authorization;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> logins = new ArrayList<>();
-        logins.add("hfhfFF");
-        logins.add("hfhfh_hfhfFF");
-        logins.add("hfhfFF77777777777777777777777777777777777");
-        logins.add("hfhFF5454");
-        logins.add("hfhfh_5454");
-        List<String> passwords = logins;
-        List<String> confirmPasswords = logins;
 
-        for (Object l : logins) {
-            for (Object p : passwords) {
-                for (Object c : confirmPasswords) {
-                    try {
-                        Authorization.checkLoginAndPasswords((String) l, (String) p, (String) c);
-                        System.out.printf("Логин: %s и пароли корректны.%n", l);
-                    } catch (WrongLoginException e) {
-                        System.out.printf("Логин \"%s\" не корректен. %s.%n", l, e.getMessage());
-                    } catch (WrongPasswordException e) {
-                        System.out.printf("Пароли \"%s\" и \"%s\" не корректны. %s.%n", p, c, e.getMessage());
-                    }
-                }
-            }
-        }
+        List<Authorization> authorizationList = new ArrayList<>();
+        authorizationList.add(new Authorization("hfhf_FF", "hfhf_FF", "hfhf_FF"));
+        authorizationList.add(new Authorization("hfhfh//hfFF", "hfhf_54FF", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhfh//hfFF", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhf_54FF", "hfhfh//hfFF"));
+        authorizationList.add(new Authorization("hfhfh//hfFF", "hfhfh//hfFF", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhfh//hfFF", "hfhf_54FF", "hfhfh//hfFF"));
+        authorizationList.add(new Authorization("hfhfh//hfFF", "hfhfh//hfFF", "hfhfh//hfFF"));
+        authorizationList.add(new Authorization("hfhfFF77777777777777777", "hfhf_54FF", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhfFF77777777777777777", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhf_54FF", "hfhfFF77777777777777777"));
+        authorizationList.add(new Authorization("hfhfFF77777777777777777", "hfhfFF77777777777777777", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhfFF77777777777777777", "hfhf_54FF", "hfhfFF77777777777777777"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhfFF77777777777777777", "hfhf_54FF"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhf_54FF", "hfhfFF77777777777777777"));
+        authorizationList.add(new Authorization("hfhf_54FF", "hfhfFF77777777777777777", "hfhfFF77777777777777777"));
+        authorizationList.add(new Authorization("hfhfFF77777777777777777", "hfhfFF77777777777777777", "hfhfFF77777777777777777"));
+
+        authorizationList.forEach(a -> Authorization.checkLoginAndPasswords(a));
     }
 }
